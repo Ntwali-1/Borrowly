f4fffc8de611233a456086295811ba69b9298270
Ntwali-1 <ntwaliyanis@gmail.com>
2025-10-16 09:08:36 +0200

    update: ProfileEntity

--- src/main/java/com/example/Borrowly/entity/Profile.java
+++ src/main/java/com/example/Borrowly/entity/Profile.java
@@ -1,4 +1,29 @@
 package com.example.Borrowly.entity;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "profiles")
 public class Profile {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne(mappedBy = "profile")
+    private User user;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String phoneNumber;
+
+    @Column(nullable = false)
+    private String address;
 }
