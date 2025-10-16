d4f0c55626854abe0327646b63ddcc6b899539e6
Ntwali-1 <ntwaliyanis@gmail.com>
2025-10-16 21:42:11 +0200

    fix: LocationEnum

--- src/main/java/com/example/Borrowly/dto/enums/Location.java
+++ src/main/java/com/example/Borrowly/dto/enums/Location.java
@@ -1,12 +1,16 @@
 package com.example.Borrowly.dto.enums;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 
 public enum Location {
+    @JsonProperty("KICUKIRO")
     KICUKIRO,
+    @JsonProperty("NYARUGENGE")
     NYARUGENGE,
+    @JsonProperty("GASABO")
     GASABO
 }
