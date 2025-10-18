b907969a2da01b44b59bf425cf15afc6dc571065
Ntwali-1 <ntwaliyanis@gmail.com>
2025-10-18 11:12:51 +0200

    update: Offer

--- src/main/java/com/example/Borrowly/entity/Offer.java
+++ src/main/java/com/example/Borrowly/entity/Offer.java
@@ -1,4 +1,15 @@
 package com.example.Borrowly.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "offers")
 public class Offer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
 }
