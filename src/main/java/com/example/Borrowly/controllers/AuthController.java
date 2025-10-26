b217d68577a1f91a29353a241ef9a140389fc054
Ntwali-1 <ntwaliyanis@gmail.com>
2025-10-26 20:32:29 +0200

    refactor: protectedRoutes

--- src/main/java/com/example/Borrowly/controllers/AuthController.java
+++ src/main/java/com/example/Borrowly/controllers/AuthController.java
@@ -3,12 +3,14 @@ package com.example.Borrowly.controllers;
 import com.example.Borrowly.dto.LoginRequest;
 import com.example.Borrowly.dto.SignupRequest;
 import com.example.Borrowly.services.AuthService;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@SecurityRequirement(name = "none")
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
