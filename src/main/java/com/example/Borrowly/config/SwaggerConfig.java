f52d652dba3125158a66e78029a1474d50b6b40c
Ntwali-1 <ntwaliyanis@gmail.com>
2025-10-26 15:50:37 +0200

    refactor: SwaggerConfig

--- src/main/java/com/example/Borrowly/config/SwaggerConfig.java
+++ src/main/java/com/example/Borrowly/config/SwaggerConfig.java
@@ -1,18 +1,33 @@
 package com.example.Borrowly.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
+
     @Bean
-    public OpenAPI borrowlyAPI() {
+    public OpenAPI customOpenAPI() {
+        final String securitySchemeName = "bearerAuth";
+
         return new OpenAPI()
                 .info(new Info()
                         .title("Borrowly API")
                         .version("1.0")
-                        .description("API documentation for Borrowly application"));
+                        .description("API documentation for Borrowly app"))
+                .addSecurityItem(new SecurityRequirement()
+                        .addList(securitySchemeName))
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName,
+                                new SecurityScheme()
+                                        .name(securitySchemeName)
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")));
     }
 }
