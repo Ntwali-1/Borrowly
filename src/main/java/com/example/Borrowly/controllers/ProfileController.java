b2d2b291dd9bf7b37de5aeab01d966c149193604
Ntwali-1 <ntwaliyanis@gmail.com>
2025-10-27 07:30:53 +0200

    refactor: OfferController

--- src/main/java/com/example/Borrowly/controllers/ProfileController.java
+++ src/main/java/com/example/Borrowly/controllers/ProfileController.java
@@ -1,15 +1,13 @@
 package com.example.Borrowly.controllers;
 
 import com.example.Borrowly.dto.ProfileRequest;
+import com.example.Borrowly.dto.UpdateProfileRequest;
 import com.example.Borrowly.services.ProfileService;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @SecurityRequirement(name = "bearerAuth")
 @RestController
@@ -26,4 +24,18 @@ public class ProfileController {
         return profileService.createProfile(profileRequest);
     }
 
+    @GetMapping("/me")
+    public ResponseEntity<?> getMyProfile() {
+        return profileService.getMyProfile();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getProfileById(@PathVariable Long id) {
+        return profileService.getProfileById(id);
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<?> updateProfile(@RequestBody UpdateProfileRequest request) {
+        return profileService.updateProfile(request);
+    }
 }
